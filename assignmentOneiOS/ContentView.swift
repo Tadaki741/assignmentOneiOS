29ec068026504e296e40b0fb923e166898b1aff2
Tadaki <tadaki741@gmail.com>
2022-07-15 16:52:21 +0700

    add Login Screen

--- assignmentOneiOS/ContentView.swift
+++ assignmentOneiOS/ContentView.swift
@@ -9,14 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var signInSuccess = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return Group {
+            if signInSuccess {
+                HomePage()
+            }
+            else {
+                LoginFormView(signInSuccess: $signInSuccess)
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct LoginFormView : View {
+    
+    @State private var userName: String = ""
+    @State private var password: String = ""
+    
+    @State private var showError = false
+    
+    @Binding var signInSuccess: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("User name")
+                TextField("type here", text: $userName)
+            }.padding()
+            
+            HStack {
+                Text(" Password")
+                TextField("type here", text: $password)
+                    .textContentType(.password)
+            }.padding()
+            
+            Button(action: {
+                // Your auth logic
+                if(self.userName == "Admin" && self.password == "Admin") {
+                    self.signInSuccess = true
+                }
+                else {
+                    self.showError = true
+                }
+                
+            }) {
+                Text("Sign in")
+            }
+            
+            if showError {
+                Text("Incorrect username/password").foregroundColor(Color.red)
+            }
+        }
     }
 }
+    
+
+
---
+++ assignmentOneiOS/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  assignmentOneiOS
+//
+//  Created by Duong Vo Dai on 15/07/2022.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text("This is the Home Page !").font(.title).foregroundColor(.green)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
