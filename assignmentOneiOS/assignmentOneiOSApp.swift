341bf1ce371d225571f3374eac7dce972e4a751c
Tadaki <tadaki741@gmail.com>
2022-07-15 15:10:48 +0700

    Initial Commit

---
+++ assignmentOneiOS/assignmentOneiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentOneiOSApp.swift
+//  assignmentOneiOS
+//
+//  Created by Duong Vo Dai on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct assignmentOneiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
