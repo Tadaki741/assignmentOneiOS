cf0fc9c9d39b0836946cb7a6df1e64fd4b53e5a8
Tadaki <tadaki741@gmail.com>
2022-07-22 14:59:32 +0700

    Try AppCode

---
+++ assignmentOneiOS/View/HomePage.swift
@@ -0,0 +1,17 @@
+//
+//  HomePage.swift
+//  assignmentOneiOS
+//
+//  Created by Duong Vo Dai on 15/07/2022.
+//
+
+import SwiftUI
+
+struct test: View {
+    let roundRect = RoundedRectangle(cornerRadius: 25.0)
+    var body: some View {
+
+        Text("This is a hello world message !");
+
+    }
+}
