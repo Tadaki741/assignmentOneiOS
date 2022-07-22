549e7c81f4110bdbe9c503d227a9c1cca2e74520
Tadaki <tadaki741@gmail.com>
2022-07-22 17:53:28 +0700

    add data for the data list

--- assignmentOneiOS/Model/DataList.swift
+++ assignmentOneiOS/Model/DataList.swift
@@ -10,12 +10,26 @@ import CoreLocation
 
 var dataList: [ProgrammingLanguage] = [
 
-    
-
-    
-
-
-
+    ProgrammingLanguage(name: "JAVA", introducedYear: "1995", nameOfAuthor: "James Gosling", countryOrigin: "USA", imageName: "java-image", application: "multi-purpose programming language"),
+    ProgrammingLanguage(name: "PYTHON", introducedYear: "1990", nameOfAuthor: "Guido van Rossum", countryOrigin: "Netherland", imageName: "python-image", application: "multi-purpose programming language"),
+    ProgrammingLanguage(name: "C++", introducedYear: "1980", nameOfAuthor: "Bjarne Stroustrup", countryOrigin: "USA", imageName: "cpp-image", application: "focused on game programming"),
+    ProgrammingLanguage(name: "C#", introducedYear: "2001", nameOfAuthor: "Anders Hejlsberg", countryOrigin: "Denmark", imageName: "csharp-image", application: "windows desktop application"),
+    ProgrammingLanguage(name: "MATLAB", introducedYear: "1984", nameOfAuthor: "Cleve Moler", countryOrigin: "USA", imageName: "matlab-image", application: "scientific calculations programming language"),
+    ProgrammingLanguage(name: "JAVASCRIPT", introducedYear: "1995", nameOfAuthor: "Brendan Eich", countryOrigin: "USA", imageName: "javascript-image", application: "web functionality programming language"),
+    ProgrammingLanguage(name: "PASCAL", introducedYear: "1970", nameOfAuthor: "Niklaus Wirth", countryOrigin: "Sweden", imageName: "pascal-image", application: "multi-purpose programming language"),
+    ProgrammingLanguage(name: "PHP", introducedYear: "1995", nameOfAuthor: "Rasmus Lerdorf", countryOrigin: "Denmark", imageName: "php-image", application: "multi-purpose scripting language"),
+    ProgrammingLanguage(name: "TYPESCRIPT", introducedYear: "2012", nameOfAuthor: "Microsoft", countryOrigin: "USA", imageName: "typescript-image", application: "server side and client side language"),
+    ProgrammingLanguage(name: "SWIFT", introducedYear: "2014", nameOfAuthor: "Apple Inc", countryOrigin: "USA", imageName: "swift-image", application: "multi-purpose programming language"),
+    ProgrammingLanguage(name: "RUBY", introducedYear: "1995", nameOfAuthor: "Yukihiro Matsumoto", countryOrigin: "Japan", imageName: "ruby-image", application: "multi-purpose programming language"),
+    ProgrammingLanguage(name: "LUA", introducedYear: "1993", nameOfAuthor: "Roberto Ierusalimschy", countryOrigin: "Brazil", imageName: "lua-image", application: "multi-paradigm programming language"),
+    ProgrammingLanguage(name: "GO", introducedYear: "2009", nameOfAuthor: "Robert Griesemer", countryOrigin: "Sweden", imageName: "go-image", application: "programming language for developing desktop application"),
+    ProgrammingLanguage(name: "PERL", introducedYear: "1987", nameOfAuthor: "Larry Wall", countryOrigin: "USA", imageName: "perl-image", application: "multi-purpose programming language"),
+    ProgrammingLanguage(name: "ADA", introducedYear: "1983", nameOfAuthor: "Jean Ichbiah", countryOrigin: "France", imageName: "ada-image", application: "high-level object-oriented programming language"),
+    ProgrammingLanguage(name: "COBOL", introducedYear: "1959", nameOfAuthor: "Jean E.Sammet", countryOrigin: "USA", imageName: "cobol-image", application: "administrative system like bank, many other businesses"),
+    ProgrammingLanguage(name: "RUST", introducedYear: "2010", nameOfAuthor: "Graydon Hoare", countryOrigin: "USA", imageName: "rust-image", application: "write operation system  or functionality for microcontroler applications"),
+    ProgrammingLanguage(name: "VISUAL BASIC", introducedYear: "1991", nameOfAuthor: "Microsoft", countryOrigin: "USA", imageName: "visualBasic-image", application: "developing windows application and to interface database system"),
+    ProgrammingLanguage(name: "HASKELL", introducedYear: "1990", nameOfAuthor: "Simon Peyton Jones", countryOrigin: "England", imageName: "haskell-image", application: "Designed for industrial application"),
+    ProgrammingLanguage(name: "R", introducedYear: "1993", nameOfAuthor: "Robert Gentleman", countryOrigin: "Canada", imageName: "r-image", application: "data analysis, data miners and statistical software")
 ]
 
 
