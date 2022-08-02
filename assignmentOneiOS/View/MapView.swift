059cfabb81bf25103b791e091109ae7d12ce9b0f
Tadaki <tadaki741@gmail.com>
2022-08-02 19:39:09 +0700

    Add map marker inside the map. Add informative text into each programming language instead of using lorem ipsum like a weirdo

--- assignmentOneiOS/View/MapView.swift
+++ assignmentOneiOS/View/MapView.swift
@@ -8,23 +8,37 @@
 import SwiftUI
 import MapKit
 
+struct Marker: Identifiable {
+    let id = UUID()
+    var location: MapMarker
+}
 
 struct MapView: View {
     
     var coordinate: CLLocationCoordinate2D;
     @State private var region = MKCoordinateRegion();
+    var markers: [Marker]
     
+    
+    //Initialize the markers list first, otherwise it will encounter an error because the self element run after the field property
+    init(coordinate: CLLocationCoordinate2D){
+        self.coordinate = coordinate;
+        markers = .init(); // <--   
+        markers.append(Marker(location: MapMarker(coordinate: coordinate, tint: .blue)))//Add the location of the country into the list
+    }
     
     var body: some View {
         
-            Map(coordinateRegion: $region)
-            .onAppear{
-                setRegion(coordinate)
-                
-            }
+        Map(coordinateRegion: $region, showsUserLocation: true,annotationItems: markers){ marker in
+            marker.location
+        }.onAppear(){
+            setRegion(coordinate)
+        }
     }
     
     private func setRegion(_ coordinate: CLLocationCoordinate2D){
-        region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 20, longitudeDelta: 20))
+        region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 30, longitudeDelta: 30))
     }
 }
+
+
