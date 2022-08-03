abba2fc4fedb8629b111b5acd92459ba2eb7dbc8
Tadaki741 <tadaki741@gmail.com>
2022-08-03 16:54:14 +0700

    Add description of the application

--- assignmentOneiOS/assignmentOneiOSApp.swift
+++ assignmentOneiOS/assignmentOneiOSApp.swift
@@ -19,14 +19,26 @@ struct assignmentOneiOSApp: App {
                 NavigationView{
                     LoginScreen()
                 }.tabItem{
-                Image(systemName: "list.bullet")
-                Text("List")
+                Image(systemName: "key")
+                Text("Login")
                 
                 }
-                Text("Profile")
-                    .tabItem{
-                        Image(systemName: "person")
-                        Text("Profile")
+                
+                
+                //Display the application description
+                VStack{
+                
+                    Text("What you can find in this app are:" )
+                    Text("1. Top programming language around the world")
+                    Text("2. Their logo")
+                    Text("3. Author who invent the language")
+                    Text("4. Main application of the language")
+                    Text("5. Country of origin where the language is published")
+                    Text("6. The year they are published")
+                    
+                }.tabItem{
+                        Image(systemName: "info.circle")
+                        Text("Application Information")
                     }
                       
             }.preferredColorScheme(isDarkMode ? .dark : .light)
