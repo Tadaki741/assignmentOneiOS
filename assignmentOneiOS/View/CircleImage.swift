7cd94b30e3cd60c22a9a64dd50e4817f05ff4dd9
Tadaki <tadaki741@gmail.com>
2022-08-05 10:30:44 +0700

    UPDATE THE HEADER COMMENT FOR EACH SOURCE CODE

--- assignmentOneiOS/View/CircleImage.swift
+++ assignmentOneiOS/View/CircleImage.swift
@@ -1,9 +1,14 @@
-//
-//  CircleImage.swift
-//  assignmentOneiOS
-//
-//  Created by Duong Vo Dai on 22/07/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Vo Dai Duong
+  ID: S3821186
+  Created  date: 31/07/2022
+  Last modified: 05/08/2022
+  Acknowledgement: Acknowledge the resources that you use here.
+*/
 
 import SwiftUI
 //Step 1 - create a circle image
--- assignmentOneiOS/assignmentOneiOSApp.swift
+++ assignmentOneiOS/assignmentOneiOSApp.swift
@@ -1,9 +1,14 @@
-//
-//  assignmentOneiOSApp.swift
-//  assignmentOneiOS
-//
-//  Created by Duong Vo Dai on 15/07/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Vo Dai Duong
+  ID: S3821186
+  Created  date: 31/07/2022
+  Last modified: 05/08/2022
+  Acknowledgement: Acknowledge the resources that you use here.
+*/
 
 import SwiftUI
 
